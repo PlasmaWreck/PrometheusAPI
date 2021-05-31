1df663b5e162bc13e52254b535a271ccc1a030f5
PlasmaWreck <31967552+PlasmaWreck@users.noreply.github.com>
2021-05-31 15:04:08 -0700

    Added Correct bool values

---
+++ Migrations/20210531220338_Changing to Bools.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrometheusAPI.Migrations
+{
+    public partial class ChangingtoBools : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "M2NVMESupport",
+                table: "MotherBoards",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "InBuiltWifi",
+                table: "MotherBoards",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NumOfFans",
+                table: "Coolers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "M2NVMESupport",
+                table: "MotherBoards",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InBuiltWifi",
+                table: "MotherBoards",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NumOfFans",
+                table: "Coolers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
--- Models/Cooler.cs
+++ Models/Cooler.cs
@@ -5,7 +5,7 @@ namespace PrometheusAPI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
-        public string NumOfFans { get; set; }
+        public int NumOfFans { get; set; }
         public bool LiquidCooling { get; set; }
         public string Price { get; set; }
         public string ImagePath { get; set; }
